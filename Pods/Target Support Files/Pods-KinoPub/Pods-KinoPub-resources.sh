58878f90323a2d9bf0ad30c3df57fefd1db8a858
Roman Gardukevich <roman_gardukevich@epam.com>
2018-01-27 10:13:01 +0300

    Update CocoaPods up to 1.4.0

--- Pods/Target Support Files/Pods-KinoPub/Pods-KinoPub-resources.sh
+++ Pods/Target Support Files/Pods-KinoPub/Pods-KinoPub-resources.sh
@@ -84,10 +84,10 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_resource "$PODS_CONFIGURATION_BUILD_DIR/EasyAbout/Settings.bundle"
+  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/EasyAbout/Settings.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_resource "$PODS_CONFIGURATION_BUILD_DIR/EasyAbout/Settings.bundle"
+  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/EasyAbout/Settings.bundle"
 fi
 
 mkdir -p "${TARGET_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
